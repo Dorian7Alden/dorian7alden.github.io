bbe262677c6b33d489414008d46671f36fc7eda1
Dorian Alden Dai <dorian7alden@gmail.com>
2026-07-02 16:12:55 +0800

    fix: 设置 lang=zh-CN 并加载 Noto Sans SC 字体

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,9 +2,13 @@ import { defineConfig } from 'vitepress'
 
 export default defineConfig({
   title: '双遥学长的个人博客',
+  lang: 'zh-CN',
   base: '/',
   head: [
     ['meta', { name: 'referrer', content: 'no-referrer' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;600;700&display=swap' }],
   ],
   themeConfig: {
     profile: {
